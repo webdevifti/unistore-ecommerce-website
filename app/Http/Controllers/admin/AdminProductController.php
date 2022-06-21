d401c007605e6e1375bf33d22ca43926cf27ca85
webdevifti <eftialam@hotmail.com>
2022-06-21 16:27:07 +0600

    product not adding

--- app/Http/Controllers/admin/AdminProductController.php
+++ app/Http/Controllers/admin/AdminProductController.php
@@ -5,7 +5,13 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\ProductRequest;
 use App\Models\Category;
 use App\Models\ProductBrand;
+use App\Models\ProductImages;
+use App\Models\Products;
 use App\Models\ProductTag;
+use App\Models\ProductTagTable;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class AdminProductController extends Controller
@@ -45,7 +51,79 @@ class AdminProductController extends Controller
     public function store(ProductRequest $request)
     {
         //
-        dd($request->all());
+        try{
+            $discount_price = ($request->discount * $request->regular_price) / 100;
+            $discounted = $request->regular_price - $discount_price;
+            $sku = rand(11111111,99999999);
+            $last_id = Products::insertGetId([
+                'category_id',
+                'brand_id',
+                'sku' => $sku,
+                'product_name' => $request->product_name,
+                'slug' => Str::slug($request->product_name),
+                'qty' => $request->qty,
+                'discount' => $request->discount,
+                'discounted_price' => $discounted,
+                'stock_alert' => 5,
+                'regular_price' => $request->regular_price,
+                'selling_price' => $request->selling_price,
+                'specification' => $request->specification,
+                'description' => $request->short_des,
+                'created_at' => Carbon::now()
+            ]);
+            if($last_id){
+                if($request->product_thumbnail){
+                    $request->validate([
+                        'product_thumbnail' => 'mimes:jpg,png,jpeg'
+                    ]);
+                    $extension = $request->product_thumbnail->getClientOriginalExtension();
+                    $imageName = $last_id.'.'.$extension;
+                    // $image_path = public_path('uploads/properties/'.$properties_id->product_thumbnail);
+                    // if (file_exists($image_path)) {
+                    //     unlink($image_path);
+                    // }
+                    $prp = Products::find($last_id);
+                    $prp->thumbnail = $imageName;
+                    $prp->save();
+                    $request->product_thumbnail->move(public_path('uploads/products/thumbnails/'), $imageName);
+                }else{
+                    return back()->with('error','Something wrong');
+                }
+                if($request->product_images)
+                {
+        
+                    foreach($request->product_images as $image)
+                    {
+                        $extension = $image->getClientOriginalExtension();
+                        $name = rand(11111,99999).'.'.$extension;
+                        $image->move(public_path().'/uploads/products/images/', $name);
+                        
+                        ProductImages::create([
+                            'product_id' => $last_id,
+                            'image' => $name
+                        ]);
+                    }
+                }else{
+                    return back()->with('error','Something wrong');
+                }
+                if($request->tags){
+                    foreach($request->tags as $tag){
+                        ProductTagTable::create([
+                            'product_id' => $last_id,
+                            'tag_id' => $tag
+                        ]);
+                    }
+                }else{
+                    return back()->with('error','Something wrong');
+                }
+               
+            }else{
+                return back()->with('error','Something wrong');
+            }
+            return back()->with('success','Product has been added successfully');
+        }catch(Exception){
+            return back()->with('error','Something wrong');
+        }
     }
 
     /**
--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -20,6 +20,12 @@
     </div>
 </div>
 <!--end breadcrumb-->
+@if(session()->has('success'))
+<div class="alert alert-success">{{ session()->get('success') }} </div>
+@endif
+@if(session()->has('error'))
+<div class="alert alert-danger">{{ session()->get('error') }} </div>
+@endif
 <div class="card">
     <div class="card-body">
         <div class="card-title">
